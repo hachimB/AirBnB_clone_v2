bbae73774a989b55673af087a671d1cbe6bf43b3
Abderrahmane369 <cszamasu@gmail.com>
2023-12-18 13:17:52 +0100

    Updated_Something

--- models/base_model.py
+++ models/base_model.py
@@ -19,12 +19,15 @@ class BaseModel:
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            if 'created_at' in kwargs and 'updated_at' in kwargs:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                         '%Y-%m-%dT%H:%M:%S.%f')
+            # del kwargs['__class__']
+            kwargs.pop('__class__', None)
             self.__dict__.update(kwargs)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
 
         storage.new(self)
 
