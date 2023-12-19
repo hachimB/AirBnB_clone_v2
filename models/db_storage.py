09ba24bdb0fa217059b2bf8505c2b1fc07980a29
Abderrahmane369 <cszamasu@gmail.com>
2023-12-19 01:19:28 +0100

    Updated_Something

--- models/db_storage.py
+++ models/db_storage.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python3
 """Module documentation"""
+
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
+from os import environ
+
+from state import State
+
+class DBStorage():
+    """Class for managing database storage"""
+    __engine = None
+    __session = sessionmaker(bind=__engine)()
+
+    def __init__(self):
+        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}".format(
+            environ.get('HBNB_MYSQL_USER'),
+            environ.get('HBNB_MYSQL_PWD'),
+            environ.get('HBNB_MYSQL_HOST'),
+            environ.get('HBNB_MYSQL_DB')
+        ), pool_pre_ping=True)
+
+        if environ.get('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        if cls:
+            objects = self.__session.query(cls).all()
+
+            _dict_ = {}
+        
+            print(objects)
 
 
+dbs = DBStorage()
+dbs.all(State)
--- models/state.py
+++ models/state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from base_model import BaseModel, Base
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
@@ -10,5 +10,18 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete-orphan', back_populates='state')
+    cities = relationship(
+        'City', cascade='all, delete-orphan', back_populates='state')
     # name = ""
+
+    @property
+    def citties(self):
+        """Getter attribute in case of file storage"""
+        from models import storage
+        from models.city import City
+        city_list = []
+        for city in storage.all(City).values():
+            if city.state_id == self.id:
+                city_list.append(City(**city))
+        return city_list
+
