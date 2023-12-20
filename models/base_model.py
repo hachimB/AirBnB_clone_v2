7823aa43e6d8cb486b88cf28b368b65b5e9a84f1
hachimB <boubacarhachim@gmail.com>
2023-12-20 15:04:04 +0100

    ok

--- models/base_model.py
+++ models/base_model.py
@@ -56,9 +56,10 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
+        # dictionary['created_at'] = self.created_at.isoformat()
+        # dictionary['updated_at'] = self.updated_at.isoformat()
         dictionary.pop('_sa_instance_state', None)
+        dictionary.pop('__class__', None)
         return dictionary
 
     def delete(self):
