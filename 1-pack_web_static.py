7d4c62492d171a50a9cb1d539b72cd527842eecb
hachimB <boubacarhachim@gmail.com>
2024-01-11 13:32:02 +0100

    ok

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 
 @task
-def do_pack(c):
+def do_pack():
     """do_pack function"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    archive = c.local(f"tar -czvf web_static_{date}.tar.gz web_static")
-    c.local("mkdir -p versions")
-    c.local(f"mv web_static_{date}.tar.gz versions")
+    archive = local(f"tar -czvf web_static_{date}.tar.gz web_static")
+    local("mkdir -p versions")
+    local(f"mv web_static_{date}.tar.gz versions")
     if archive.ok:
         return f"versions/web_static_{date}.tar.gz"
     else:
