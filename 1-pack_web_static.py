911c19be35e2e2ad80746ae7bf70e7b67a815a35
hachimB <boubacarhachim@gmail.com>
2024-01-11 09:44:30 -0800

    ok

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Module documentation"""
-from fabric.api import local, task
+from fabric.api import local
 from datetime import datetime
 
 
@@ -8,7 +8,7 @@ def do_pack():
     """do_pack function"""
     date = datetime.now().strftime("%Y%m%d%H%M%S")
     local("mkdir -p versions")
-    arch = local(f"tar -czvf versions/web_static_{date}.tar.gz web_static")
+    arch = local(f"tar -czvf versions/web_static_{date}.tgz web_static")
     if arch.failed:
         return None
     else:
