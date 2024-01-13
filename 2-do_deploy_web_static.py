2ed168702507c1ad132b30b1e9b7536078b22ce3
hachimB <boubacarhachim@gmail.com>
2024-01-13 13:19:56 -0800

    ok

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -17,16 +17,15 @@ def do_deploy(archive_path):
         archive_name = os.path.basename(archive_path)
         archive_name_without_ext = os.path.splitext(os.path.basename
                                                     (archive_path))[0]
-        put(archive_path, "/tmp/{}".format(archive_name_without_ext))
+        put(archive_path, "/tmp/{}".format(archive_name))
         run("mkdir -p /data/web_static/releases/{}".format
             (archive_name_without_ext))
         run("tar -xvf /tmp/{} -C /data/web_static/releases/{}".format
-            (archive_name_without_ext, archive_name_without_ext))
-        run("rm -rf /tmp/{}".format(archive_name_without_ext))
+            (archive_name, archive_name_without_ext))
+        run("rm -rf /tmp/{}".format(archive_name))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{} /data/web_static/current"
             .format(archive_name_without_ext))
     except Exception as e:
-        print(e)
         return False
     return True
