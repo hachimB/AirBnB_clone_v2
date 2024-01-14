de87bd651bca7ae9264eb8cd17d825ba67405793
hachimB <boubacarhachim@gmail.com>
2024-01-14 03:16:35 -0800

    ok

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -21,9 +21,9 @@ def do_deploy(archive_path):
             (archive_name_without_ext))
         run("tar -xvf /tmp/{} -C /data/web_static/releases/{}".format
             (archive_name, archive_name_without_ext))
-        run("rm -rf /tmp/{}".format(archive_name))
+        run("rm  /tmp/{}".format(archive_name))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+        run("ln -sf /data/web_static/releases/{} /data/web_static/current"
             .format(archive_name_without_ext))
         return True
     except Exception as e:
