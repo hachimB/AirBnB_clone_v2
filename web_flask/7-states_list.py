a2d4a3cda537a2fad1d3e7ce862e3285d79f1d97
hachimB <boubacarhachim@gmail.com>
2024-01-26 22:51:57 +0100

    ok

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,10 +16,7 @@ def close_storage(exception):
 def states_list():
     """states"""
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda x: x.name)
-    return render_template('7-states_list.html', slist=sorted_states)
-    #states = storage.all(State)
-    #return render_template('7-states_list.html', slist=states)
+    return render_template('7-states_list.html', slist=states)
 
 
 if __name__ == '__main__':
