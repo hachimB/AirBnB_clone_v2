7552e798f18b51e623210021e5519fd66822a2cd
hachimB <boubacarhachim@gmail.com>
2024-01-26 18:57:29 +0100

    states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Modules documentation"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_storage():
+    """close storage"""
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', slist=states)
