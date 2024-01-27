60ebad8e2b28c4daa30d77040b4aa67f2454d304
hachimB <boubacarhachim@gmail.com>
2024-01-27 18:12:04 +0100

    ok

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -23,7 +23,7 @@ def states():
         if storage_type == 'db':
             cities = s.cities
         else:
-            cities = s.cities()
+            cities = s.cities
         s.all_cities = cities
     return render_template('9-states.html', states=all_states)
 
