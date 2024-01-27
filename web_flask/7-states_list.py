f9d673fc7d8dfb33c4f99e2a334ce3b584fa3f65
hachimB <boubacarhachim@gmail.com>
2024-01-27 01:01:04 +0100

    ok

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,17 +6,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def close_storage(exception):
-    """close storage"""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """states"""
     states = storage.all(State)
     return render_template('7-states_list.html', slist=states)
+
+
+@app.teardown_appcontext
+def close_storage(exception):
+    """close storage"""
+    storage.close()
 
 
 if __name__ == '__main__':
